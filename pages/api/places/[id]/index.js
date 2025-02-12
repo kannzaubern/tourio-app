1570c3c6dc06513a929fd182244ebb7eac202e10
Stephanie Preuthen <kann.zaubern@gmail.com>
2025-02-12 15:29:35 +0100

    add read data

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,20 @@
-import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Places";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
+  console.log("Id", id);
 
-  const place = places.find((place) => place.id === id);
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
 
-  if (!place) {
-    response.status(404).json({ status: "Not found" });
+    if (!place) {
+      response.status(404).json({ status: "Not Found" });
+      return;
+    }
+    response.status(200).json(place);
     return;
   }
-
-  response.status(200).json(place);
+  response.status(405).json({ status: "Method not allowed" });
 }
