08ac79ff219a1732e3f2b8a6b787e4f7bbdd06f3
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-02-12 16:52:28 +0100

    PUT wip

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,20 +1,34 @@
-import dbConnect from "@/db/connect";
-import Place from "@/db/models/Places";
+import dbConnect from '@/db/connect';
+import Place from '@/db/models/Places';
 
 export default async function handler(request, response) {
-  await dbConnect();
-  const { id } = request.query;
-  console.log("Id", id);
+    await dbConnect();
+    const { id } = request.query;
 
-  if (request.method === "GET") {
-    const place = await Place.findById(id);
+    try {
+        switch (request.method) {
+            case 'PUT':
+                const updatePlace = request.body;
+                console.log('Updating place request.');
+                await Place.findByIdAndUpdate(id, updatePlace);
+                response.status(200).json({ status: 'Place was updated' });
 
-    if (!place) {
-      response.status(404).json({ status: "Not Found" });
-      return;
+                break;
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({ status: 'Method not allowed.' });
     }
-    response.status(200).json(place);
-    return;
-  }
-  response.status(405).json({ status: "Method not allowed" });
+
+    // if (request.method === "GET") {
+    //   const place = await Place.findById(id);
+
+    //   if (!place) {
+    //     response.status(404).json({ status: "Not Found" });
+    //     return;
+    //   }
+    //   response.status(200).json(place);
+    //   return;
+    // }
+    // response.status(405).json({ status: "Method not allowed" });
 }
